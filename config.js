0d40936f88189c8949297f55e3a946ac73ba970f
safiscorport <safiscorport@gmail.com>
2026-09-11 21:54:11 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ window.SHARED_HISTORY_CONFIG = {
   // Paste the deployed Google Apps Script Web App /exec URL here.
   APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxe0sIAgtBITEMXSlZ8FpXCAObLa1bSkPn11AFVSNvqj0_BG-DtbyfbYByjUALWsZpm/exec",
 
-  RETENTION_DAYS: 7,
+  RETENTION_DAYS: 30,
   SAVE_INTERVAL_MS: 300000
 };
